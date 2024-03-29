eb1bcda67aefd9abe42b8924eb002c5037d52cd5
Evgereo <world.evgereo@gmail.com>
2024-03-29 17:30:50 +0300

    init

---
+++ gateway/src/test/java/world/evgereo/verbverse/userservice/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package world.evgereo.verbverse.userservice.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
